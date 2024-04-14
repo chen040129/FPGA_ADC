a0d5f124bf847cbb34fa2d8fa332d9ec4329656b
chen-songyun0129 <2642906810@qq.com>
2024-04-14 17:20:41 +0800

    发送端最终代码

--- uart.v
+++ uart.v
@@ -14,6 +14,7 @@ reg [7:0] arry[15:0];//输出信号
 reg [7:0] arr[15:0];//输出信号
 reg [7:0] test;
 integer n;
+integer My_begin=0;
 
 assign out=arry[0];
 assign test_put=i[0];
@@ -28,6 +29,9 @@ clk(
 
 always @(negedge uart_busy)//设置移位的数量
 begin
+
+	if(!RST_n)
+	begin
 		arry[0]  <= "h";
 		arry[1]	<= "e";
 		arry[2]  <= "l";
@@ -40,48 +44,40 @@ begin
 		arry[9]  <= "l";
 		arry[10] <= "d";
 		arry[11]	<= 8'h0A;
-		for(n=0;n<12;n=n+1)
-		begin
-			arr[n] <= arry[n];
-		end
-	if(!RST_n)
-	begin
 		i <= 4'd0;
+		$deposit(arr, arry);
 	end
 	else
 	begin
-		i <= i + 1'b1;
 		if(arry[i]==8'h0A)
 		begin
 			i<=0;
-			for(n=0;n<12;n=n+1)
-			begin
-				arry[n] <= arr[n];
-			end
+			$deposit(arry, arr);
 		end
+		i <= i + 1'b1;
 	end
 end
 
-uart_tx(
+//uart_tx(
+//	.RST_clk        (RST_clk),
+//	.RST_n          (RST_n),
+//	.tx_data			 (arry[i]),//传入的8位数据，需要可以修改
+//	.uart_tx_data   (uart_tx_data),//输出的接口
+//	.uart_busy      (uart_busy)//传输完一个数据的判断
+//);
+
+
+
+
+
+SPI_tx(
 	.RST_clk        (RST_clk),
 	.RST_n          (RST_n),
 	.tx_data			 (arry[i]),//传入的8位数据，需要可以修改
-	.uart_tx_data   (uart_tx_data),//输出的接口
-	.uart_busy      (uart_busy)//传输完一个数据的判断
+	.spi_tx_data   (uart_tx_data),//输出的接口
+	.spi_busy      (uart_busy),//传输完一个数据的判断
+	.spi_clk        (clk_out)
 );
-
-
-
-
-
-//SPI_tx(
-//	.RST_clk        (RST_clk),
-//	.RST_n          (RST_n),
-//	.tx_data			 (8'b10100110),//传入的8位数据，需要可以修改
-//	.spi_tx_data   (uart_tx_data),//输出的接口
-//	.spi_busy      (uart_busy),//传输完一个数据的判断
-//	.spi_clk        (clk_out)
-//);
 
 
 
